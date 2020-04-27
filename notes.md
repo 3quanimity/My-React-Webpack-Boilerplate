7252386c7e9451e549a0afa49186e7371a3261bf
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-04-27 02:40:28 +0100

    first push

---
+++ notes.md
@@ -0,0 +1,25 @@
+- no npx install
+
+- using babel to compile app.js in public/scripts
+
+- conditional rendering
+  1. if statement in a function
+  2. ternary operator
+  3. logical "AND" operator
+
+| Props                                 | State                              |
+| ------------------------------------- | ---------------------------------- |
+| An object                             | An object                          |
+| Can be used when rendering            | Can be used when rendering         |
+| Changes (from above) cause re-renders | Can cause re-renders               |
+| Comes from above                      | Defined in component itself        |
+| Can't be changed by component itself  | Can be changed by component itself |
+
+- storing data to local storage
+
+- webpack
+- babel
+
+- React Modal
+
+- sass for styling
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  // MINIMAL WEBPACK SETUP: entry point & output final bundle file
+  entry: "./src/app.js", // relative path
+  output: {
+    path: path.join(__dirname, "public"), // absolute path
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // Loader : customise webpack behaviour when loading a file (convert down jsx --to-> js)
+        loader: "babel-loader",
+        test: /\.js$/, // regular expression to target only files ending with ".js"
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          // allow the use of multiple loaders (in an array)
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  // Source Map
+  devtool: "eval-cheap-source-map",
+  // Dev Server
+  devServer: {
+    contentBase: path.join(__dirname, "public"), // absolute path
+  },
+};
